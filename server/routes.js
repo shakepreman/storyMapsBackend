f77a48fc7f947a38d6835b1542f93cdc39a143e7
Abhishek Preman <apreman@us.ibm.com>
2016-04-13 19:20:29 -0400

    Allowing cors on routes

--- server/routes.js
+++ server/routes.js
@@ -9,12 +9,17 @@ var path = require('path');
 
 module.exports = function(app) {
 
-  app.all('/', function(req, res, next) {
+  app.all('/api/locations', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next()
   });
 
+  app.all('/api/stories', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
   // Insert routes below
   app.use('/api/locations', require('./api/location'));
   app.use('/api/stories', require('./api/stories'));
